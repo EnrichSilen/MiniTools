27878381fad8a913d177e5a35266fc32b25b3942
Enrich <enrich.silen@gmail.com>
2017-06-05 21:23:54 +0200

    off & su

---
+++ off/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace off
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process p = new Process();
+
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.WorkingDirectory = Environment.CurrentDirectory;
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.FileName = "cmd";
+            
+            try
+            {
+                p.Start();
+                if(args.Length > 0)
+                    p.StandardInput.WriteLine("shutdown /s /t " + args[0]);
+                else
+                    p.StandardInput.WriteLine("shutdown /s /t 120");
+            }
+            catch (Exception ex)
+            {
+                Console.Clear();
+                Console.WriteLine(ex.ToString());
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ su/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace su
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessStartInfo proc = new ProcessStartInfo();
+            proc.UseShellExecute = true;
+            proc.WorkingDirectory = Environment.CurrentDirectory;
+            proc.CreateNoWindow = false;
+            proc.FileName = "cmd";
+            proc.Verb = "runas";
+            try
+            {
+                Process.Start(proc);
+            }
+            catch (Exception ex)
+            {
+                Console.Clear();
+                Console.WriteLine(ex.ToString());
+                Console.ReadKey();
+            }
+        }
+    }
+}
