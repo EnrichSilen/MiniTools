6ebdae40d93293df55b35c71a82716cd7584d141
Enrich <enrich.silen@gmail.com>
2017-07-17 21:17:54 +0200

    build rename

--- ipwatcher/Properties/AssemblyInfo.cs
+++ ipwatcher/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Obecné informace o sestavení se řídí přes následující 
 // sadu atributů. Změnou hodnot těchto atributů se upraví informace
 // přidružené k sestavení.
-[assembly: AssemblyTitle("ipw")]
+[assembly: AssemblyTitle("IP Watcher")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ipwatcher")]
+[assembly: AssemblyProduct("IP Watcher")]
 [assembly: AssemblyCopyright("Enrich Silen © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
