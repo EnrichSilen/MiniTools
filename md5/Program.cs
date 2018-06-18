49b832e9b59e9713e0e0783b323a3dea2af6a65b
Enrich Silen <enrich.silen@gmail.com>
2018-06-19 01:40:16 +0200

    MD5 project folder

---
+++ md5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace md5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("invalid or no input");
+                return;
+            }
+            byte[] hash;
+            using (var md5 = new MD5Cng())
+                hash = md5.ComputeHash(Encoding.UTF8.GetBytes(args[0]));
+            Console.WriteLine(BitConverter.ToString(hash).Replace("-", ""));
+        }
+    }
+}
